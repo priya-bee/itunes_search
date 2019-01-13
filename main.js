2fb7f6e24fc8ab5a20abe5ab38e5ef34b83e3e54
Priya Bhatnagar <priyabhatnagar@Priyas-MacBook-Pro.local>
2019-01-12 16:40:50 -0800

    created Vue instance

---
+++ main.js
@@ -0,0 +1,7 @@
+// Create a Vue instance
+var app = new Vue({ // options go here 
+	el: '#app', // plug our instance of the element in DOM with the id 'app'
+	data: {
+		search: 'Search bar goes here'
+	}
+})
