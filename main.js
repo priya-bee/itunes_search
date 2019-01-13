4dd71611f190ee6368458d65f2aa72e915deb826
Priya Bhatnagar <priyabhatnagar@Priyas-MacBook-Pro.local>
2019-01-12 18:32:27 -0800

    fetching itunes api results in main, need to abstract

--- main.js
+++ main.js
@@ -2,6 +2,22 @@
 var app = new Vue({ // options go here 
 	el: '#app', // plug our instance of the element in DOM with the id 'app'
 	data: {
-		searchTerm: ''
-	}
+		searchTerm: '',
+		myMedia: ''
+	},
+	methods: {
+	    getResults: function(s){
+	    	var myScope = this;
+		    fetch('https://itunes.apple.com/search?term='+s)
+		      .then( response => response.json()) 
+		      .then((data) => {
+		      	myScope.myMedia = data; // parse this stuff
+		      })
+		},
+		// clear the cache and the view
+		clear: function(){
+			this.myMedia = '';
+			this.searchTerm = '';
+		}
+  	}
 })
